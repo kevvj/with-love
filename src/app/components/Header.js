6a42e226678789c05e684f4342cb8d3afd89f9b4
kevvj <kevinjvillalba774+1@gmail.com>
2024-12-20 14:03:59 -0500

    FirstCommit:Header

---
+++ src/app/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () =>{
+    return(
+        <div className="Header">
+            <p>WithLove</p>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Servicios</li>
+                <li>Galeria</li>
+                <li>Contacto</li>
+            </ul>        
+        </div>
+    )
+}
+
+export default Header
+
