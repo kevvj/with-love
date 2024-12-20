5f21d8d73ccb0b718b20674bdbaafbaf0697f1db
kevvj <kevinjvillalba774+1@gmail.com>
2024-12-20 18:54:39 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,12 @@
 'use client'
-
-import { useEffect, useState } from "react"
 import Header from "./components/Header"
 
-
-
 export default function Home() {
-
-  const [num, setNum] = useState(0)
-
-
-
+  
   return (
     <>
       <Header />
     </>
-
   )
 }
 
