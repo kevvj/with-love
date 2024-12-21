f515ae70b9c753e3975e30a46c6e8e1569fed653
kevvj <kevinjvillalba774+1@gmail.com>
2024-12-20 22:30:24 -0500

    b

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const ContactSection = () => {
     return (
@@ -10,6 +11,8 @@ const ContactSection = () => {
                     <h1> Monteria</h1>
                     <h2><FontAwesomeIcon icon={faPhone} size="1x" color="black"/>   +57 321 6301533</h2>
                     <h2><FontAwesomeIcon icon={faInstagram} size="1x" color="black"/> decoraciondeeventoswithlove</h2>
+
+                    <h2><FontAwesomeIcon icon={faFacebook} size="1x" color="black"/> Creatividad y Eventos With Love</h2>
                 </div>
 
             </div>
