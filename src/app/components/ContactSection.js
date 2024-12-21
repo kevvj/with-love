1ad6161662536fa65b65bf76ccae87cbed5ce17d
kevvj <kevinjvillalba774+1@gmail.com>
2024-12-20 23:20:59 -0500

    cosas de carga para que vaya mejor

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -1,22 +1,39 @@
+'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { useEffect, useState } from 'react';
 
 const ContactSection = () => {
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
     return (
         <div className="contact-section-container">
-            <div className="contact-section">
+            <div className={isClient ? "contact-section" : "contact-section-spinner"}>
                 <div className="contact-content">
                     <h1> Monteria</h1>
-                    <a href='' target="_blank"><FontAwesomeIcon icon={faPhone} size="1x" color="black"/>   <h2> +57 321 6301533</h2></a>
+                    <a href='' target="_blank">
+                        {isClient && <FontAwesomeIcon icon={faPhone} size="1x" color="black" />}
 
-                    <a href='' target="_blank"><FontAwesomeIcon icon={faInstagram} size="1x" color="black"/> <h2> decoraciondeeventoswithlove</h2></a>
+                        <h2> +57 321 6301533</h2></a>
 
-                    <a href='https://www.facebook.com/creatividadwithlove?mibextid=ZbWKwL' target="_blank"><FontAwesomeIcon icon={faFacebook} size="1x" color="black"/> <h2> Creatividad y Eventos With Love</h2></a>
+                    <a href='https://www.instagram.com/decoraciondeeventoswithlove?igsh=MThiMjc5NGV0NjMzag==' target="_blank">
+                        {isClient && <FontAwesomeIcon icon={faInstagram} size="1x" color="black" />}
+
+                        <h2> decoraciondeeventoswithlove</h2></a>
+
+                    <a href='https://www.facebook.com/creatividadwithlove?mibextid=ZbWKwL' target="_blank">
+                        {isClient && <FontAwesomeIcon icon={faFacebook} size="1x" color="black" />}
+
+                        <h2> Creatividad y Eventos With Love</h2></a>
                 </div>
 
             </div>
+
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 'use client'
 import Header from "./components/Header"
 import ContactSection from "./components/ContactSection"
-
+import { ClipLoader } from "react-spinners";
 export default function Home() {
 
   return (
     <>
       <Header />
       <ContactSection></ContactSection>
+      
     </>
   )
 }
