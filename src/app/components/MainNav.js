52c3b9e56f4ee018f09efcb42e2df93f368bab77
kevvj <kevinjvillalba774+1@gmail.com>
2025-01-31 10:26:11 -0500

    soy preso de la carcel de tus besos

--- src/app/components/MainNav.js
+++ src/app/components/MainNav.js
@@ -1,4 +1,9 @@
 const MainNav = () => {
+
+    const listnav = [
+        { id: 1, title: "Ejemplo", text: "texto de Ejemplo con todo y textoasodiq", color: "#a767db" },
+        { id: 2, title: "Ejemplo", text: "texto de Ejemplo con todo y textoasodiq", color: "#ffa1d2" },
+        { id: 3, title: "Ejemplo", text: "texto de Ejemplo con todo y textoasodiq", color: "#c1ff72" },]
     return (
         <div className="main-nav-container">
             <div className="main-nav-img">
@@ -8,18 +13,12 @@ const MainNav = () => {
             </div>
 
             <div className="main-nav">
-                <div className="main-nav-list">
-                    <h2>Ejemplo</h2>
-                    <p>texto de Ejemplo con todo y textoasodiq</p>
-                </div>
-                <div className="main-nav-list">
-                    <h2>Ejemplo</h2>
-                    <p>texto de Ejemplo con todo y textoasodiq</p>
-                </div>
-                <div className="main-nav-list">
-                    <h2>Ejemplo</h2>
-                    <p>texto de Ejemplo con todo y textoasodiq</p>
-                </div>
+                {listnav.map(list => (
+                    <div className="main-nav-list" key={list.id} style={{backgroundColor: list.color}}>
+                        <h2>{list.title}</h2>
+                        <p>{list.text}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
