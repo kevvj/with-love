8b0b3b6c34e8cdeb87fbe657b40aded21fcff03b
Anuarsin <anuardavid2005@gmail.com>
2025-03-28 13:43:36 -0500

    colorMapContact:change

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -6,36 +6,33 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { useEffect, useState } from 'react';
 
 const ContactSection = () => {
-    const [isClient, setIsClient] = useState(false)
+    const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
-        setIsClient(true)
-    }, [])
+        setIsClient(true);
+    }, []);
+
     return (
         <div className="contact-section-container">
             <div className={isClient ? "contact-section" : "contact-section-spinner"}>
                 <div className="contact-content">
-                    <h1>Contactanos</h1>
-                    <a target="_blank">
-                        {isClient && <FontAwesomeIcon icon={faPhone} size="1x" color="black" />}
-
-                        <h2> +57 321 6301533</h2></a>
-
-                    <a href='https://www.instagram.com/decoraciondeeventoswithlove?igsh=MThiMjc5NGV0NjMzag==' target="_blank">
-                        {isClient && <FontAwesomeIcon icon={faInstagram} size="1x" color="black" />}
-
-                        <h2> decoraciondeeventoswithlove</h2></a>
-
-                    <a href='https://www.facebook.com/creatividadwithlove?mibextid=ZbWKwL' target="_blank">
-                        {isClient && <FontAwesomeIcon icon={faFacebook} size="1x" color="black" />}
-
-                        <h2> Creatividad y Eventos With Love</h2></a>
+                    <h1>Contáctanos</h1>
+                    <a href="tel:+573216301533" target="_blank" className="contact-link">
+                        {isClient && <FontAwesomeIcon icon={faPhone} size="1x" className="icon" />}
+                        <h2>+57 321 6301533</h2>
+                    </a>
+                    <a href='https://www.instagram.com/decoraciondeeventoswithlove' target="_blank" className="contact-link">
+                        {isClient && <FontAwesomeIcon icon={faInstagram} size="1x" className="icon" />}
+                        <h2>@decoraciondeeventoswithlove</h2>
+                    </a>
+                    <a href='https://www.facebook.com/creatividadwithlove' target="_blank" className="contact-link">
+                        {isClient && <FontAwesomeIcon icon={faFacebook} size="1x" className="icon" />}
+                        <h2>Creatividad y Eventos With Love</h2>
+                    </a>
                 </div>
-
             </div>
-
         </div>
-    )
+    );
 }
 
-export default ContactSection
+export default ContactSection;
--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -2,13 +2,13 @@ const Map = () => {
     return (
         <div className="map-container">
             <div className="map">
-                <h2>Encuentranos en</h2>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63095.41465723531!2d-75.88214288351266!3d8.742392347707074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e5a2fe5a57031ad%3A0x92e1cbad2ed7c0a0!2zTW9udGVyw61hLCBDw7NyZG9iYQ!5e0!3m2!1ses!2sco!4v1736642455942!5m2!1ses!2sco"
-                    className="iframe-map"
-                ></iframe>
-
-
+                <h2>Encuéntranos en</h2>
+                <div className="map-box">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63095.41465723531!2d-75.88214288351266!3d8.742392347707074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e5a2fe5a57031ad%3A0x92e1cbad2ed7c0a0!2zTW9udGVyw61hLCBDw7NyZG9iYQ!5e0!3m2!1ses!2sco!4v1736642455942!5m2!1ses!2sco"
+                        className="iframe-map"
+                    ></iframe>
+                </div>
             </div>
         </div>
     );
