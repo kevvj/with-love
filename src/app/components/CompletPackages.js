326002cb5950703440242b7047e55820ce1409e7
kevvj <kevinjvillalba774+1@gmail.com>
2025-03-31 02:48:33 -0500

    .

--- src/app/components/CompletPackages.js
+++ src/app/components/CompletPackages.js
@@ -9,58 +9,38 @@ const CompletPackages = () => {
             titulo: "Fiestas Divertidas",
             descripcion:
                 "Paquetes de decoración listos para usar.",
-            imagen: "",
+            image: "/—Pngtree—cute birthday animals illustration_5331094.png",
             color: "#FF6A6F",
             icon: faBomb,
             rute: "/complete-packages",
-            detalles: [
-                "Decoraciones tematicas completas",
-                "Animador profesional",
-                "karaoke concursos",
-                "Maquillaje infantil",
-                "Piñatas personalizadas"
-            ]
+            
 
         },
         {
             id: 2,
             titulo: "Super celebraciones",
             descripcion: "Decoraciones únicas para cada ocasión.",
-            imagen: "",
+            image: "/happy-birthday-cartoons_24640-27310.jpg",
             color: "#72FF35",
             icon: faGift,
             rute: "/theme-decorations",
-            detalles: [
-                "Decoraciones tematicas completas",
-                "Animador profesional",
-                "karaoke concursos",
-                "Maquillaje infantil",
-                "Piñatas personalizadas"
-            ]
         },
         {
             id: 3,
             titulo: "Fiestas espectaculares",
             descripcion:
                 "Crea un paquete segun tus preferencias",
-            imagen: "",
+            image: "/hand-drawn-confetti-drawing-illustration_23-2150918176.jpg",
             color: "#FFCB22",
             icon: faWandMagicSparkles,
             rute: "/make-it-yours",
-            detalles: [
-                "Decoraciones tematicas completas",
-                "Animador profesional",
-                "karaoke concursos",
-                "Maquillaje infantil",
-                "Piñatas personalizadas"
-            ]
         },
 
     ];
 
     return (
         <div className="complete-complete">
-             <h2 className="completepackages-title">Paquetes Completos</h2>
+             <h2 className="completepackages-title">Paquetes completos para cada ocación</h2>
 
         <div className="completepackages-container">
            
@@ -69,27 +49,22 @@ const CompletPackages = () => {
                 <div
                     key={completepackages.id}
                     className="completepackages"
-                    style={{ backgroundColor: completepackages.color }}
+                    style={{borderColor: completepackages.color}}
                 >
 
-
-                    {/* <img
-                  alt={completepackages.titulo}
-                  className="completepackages-imagen"
-                /> */}
-                    <div className="completepackages-icon">
-                        <FontAwesomeIcon size="1x" color="#white" icon={completepackages.icon}></FontAwesomeIcon>
-                    </div>
+                    
 
 
                     <div className="completepackages-text">
-                        <h2 className="completepackages-titulo">{completepackages.titulo}</h2>
+                        <h2 style={{color: completepackages.color, fontSize: "1.3rem"}}>Paquete</h2>
+                        <h2 style={{color: completepackages.color}} className="completepackages-titulo" >{completepackages.titulo}</h2>
+                        <img src={completepackages.image}></img>
                         <p className="completepackages-descripcion">{completepackages.descripcion}</p>
-                        <ul className="completepackages-detalles">
-                            {completepackages.detalles.map((detalle, index) => (
-                                <li key={index}>{detalle}</li>
-                            ))}
-                        </ul>
+
+                        <p className="completepackages-price">$130</p>
+
+                        <button className="completepackages-button" style={{backgroundColor: completepackages.color}}>Ver más</button>
+                        
                     </div>
 
 
